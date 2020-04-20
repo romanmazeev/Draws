f2af7de81a9cb356b19cbfb3cec51c7a0759cf09
Roman Mazeev <centr82009@gmail.com>
2020-04-20 03:52:05 +0300

    Update data_preporation script

--- Model/data_preporation.py
+++ Model/data_preporation.py
@@ -1,22 +1,22 @@
 import turicreate as tc
 import numpy as np
 import os
-import json
 
-random_state = np.random.RandomState(100)
+random_state = np.random.RandomState(11000)
 
 # Change if applicable
-quickdraw_dir = 'quickdraw'
-bitmaps_dir = os.path.join(quickdraw_dir, 'bitmaps')
-strokes_dir = os.path.join(quickdraw_dir, 'strokes')
-sframes_dir = os.path.join(quickdraw_dir, 'sframes')
-npy_ext = '.npz'
-ndjson_ext = '.ndjson'
-num_examples_per_class = 100
-classes = ["square", "triangle"]
+dataset_dir = 'Dataset'
+bitmaps_dir = os.path.join(dataset_dir, 'bitmaps')
+npy_ext = '.npy'
+num_examples_per_class = 11000
+
+f = open('classes.txt', 'r')
+classes = f.read().splitlines()
+f.close()
+
 num_classes = len(classes)
 
-def build_bitmap_sframe():
+def build_sframe():
     bitmaps_list, labels_list = [], []
     for class_name in classes:
         class_data = np.load(os.path.join(bitmaps_dir, class_name + npy_ext))
@@ -36,37 +36,7 @@ def build_bitmap_sframe():
             labels_list.append(class_name)
 
     sf = tc.SFrame({"drawing": bitmaps_list, "label": labels_list})
-    sf.save(os.path.join(sframes_dir, "bitmap_quickdraw.sframe"))
-    return sf 
-    
-def build_strokes_sframe():
-    drawings_list, labels_list = [], []
-    for class_name in classes:
-        with open(os.path.join(strokes_dir, class_name+ndjson_ext)) as fin:
-            ndjson_data = list(map(lambda x: x.strip(), fin.readlines()))
-        random_state.shuffle(ndjson_data)
-        ndjson_data_selected = list(map(json.loads, ndjson_data[:num_examples_per_class]))
-        raw_drawing_list = [ndjson["drawing"] for ndjson in ndjson_data_selected]
-        def raw_to_final(raw_drawing):
-            return [
-                [
-                    {
-                        "x": raw_drawing[stroke_id][0][i], 
-                        "y": raw_drawing[stroke_id][1][i]
-                    } for i in range(len(raw_drawing[stroke_id][0]))
-                ] 
-                for stroke_id in range(len(raw_drawing))
-            ]
-
-        final_drawing_list = list(map(raw_to_final, raw_drawing_list))
-        drawings_list.extend(final_drawing_list)
-        labels_list.extend([class_name] * num_examples_per_class)
-    sf = tc.SFrame({"drawing": drawings_list, "label": labels_list})
-    sf.save(os.path.join(sframes_dir, "stroke_quickdraw.sframe"))
+    sf.save(os.path.join(dataset_dir, "drawn.sframe"))
     return sf 
 
-sf = build_bitmap_sframe()
-sf = build_strokes_sframe()
-
-sf["rendered"] = tc.drawing_classifier.util.draw_strokes(sf["drawing"])
-sf.explore()
+sf = build_sframe()
